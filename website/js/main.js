f6f44e0bedd34942c11ee02e927d845e1d882881
LenaKari <olena.levitsky@gmail.com>
2015-09-12 00:23:42 +0100

    Added visualisations, loading dialogue and general css edits

--- website/js/main.js
+++ website/js/main.js
@@ -1,23 +1,4 @@
-var navigateAnimations = function(display, animation) {
-  if(display) {
-    $('#animation-container-' + animation).show();
-    $('#code-container-' + animation).show();
-    $('.data-vis-subnav-' + animation).attr('id', 'active-data-vis-button');
-  } else {
-    $('#animation-container-' + animation).hide();
-    $('#code-container-' + animation).hide();
-    $('.data-vis-subnav-' + animation).removeAttr('id');
-  }
-};
-
-var loadAnimation = function(animation) {
-  var count = 0;
-  for(var i=1; i <= 6; i++) {
-    if(animation == count) {
-      navigateAnimations(true, i-1);
-    } else {
-      navigateAnimations(false, i-1);
-    }
-    count++;
-  }
-};
+// Hide loader when animation has loaded
+jQuery(window).load(function () {
+      $('.loading').hide();
+});
