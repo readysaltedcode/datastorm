3df79ffa7f378ffa6ce5991540f42256b431ce88
Nat Smith <nat.smith@snappersystems.co.uk>
2016-01-26 16:23:20 +0000

    Remove website
    Moved to datadrivendance repo

--- datadrivendance/datastorm/js/datavisheight.js
+++
@@ -1,8 +0,0 @@
-var screenWidth = window.innerWidth;
-if (screenWidth > 600 && screenWidth < 1000) {
-    alert("Medium");
-    document.getElementById("codepen").data-height="400";
-} else if (screenWidth < 601) {
-    alert("Small");
-    document.getElementById("codepen").data-height="200";
-};
--- datadrivendance/js/main.js
+++
@@ -1,4 +0,0 @@
-// Hide loader when animation has loaded
-jQuery(window).load(function () {
-      $('.loading').hide();
-});
